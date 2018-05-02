4ce53c8e812e7a22b98bb34156edff2ede57ac67
Paula Garcia <paula.gfernandez@alumnos.upm.es>
2018-05-03 00:10:14 +0200

    paso6

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var partials = require('express-partials');
+var methodOverride = require('method-override');
+
+
 var index = require('./routes/index');
 
 var app = express();
@@ -13,7 +16,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(partials());
 
 
 
@@ -24,7 +26,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(methodOverride('method',{methods: ["POST", "GET"]}));
+app.use(partials());
 app.use('/', index);
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const quizController = require('../controllers/quiz_controllers');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,8 +12,22 @@ router.get('/', function(req, res, next) {
 router.get('/credits',function(req,res,next){
   res.render('credits');
 });
-router.get('/quizzes',function(req,res,next){
-    res.render('quizzes');
-});
+
+router.get('/quizzes',                     quizController.index);
+router.get('/quizzes/:quizId(\\d+)',       quizController.show);
+router.get('/quizzes/new',                 quizController.new);
+router.post('/quizzes',                    quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',       quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
+
+router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
+router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+
+
+
+//router.get('/quizzes',function(req,res,next){
+  //  res.render('quizzes');
+//});
 
 module.exports = router;
