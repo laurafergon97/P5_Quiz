c4086ce7a648aca576c64d71a33f65b349122db5
Paula Garcia <paula.gfernandez@alumnos.upm.es>
2018-05-03 12:28:14 +0200

    sqlite3

--- models/index.js
+++ models/index.js
@@ -1,9 +1,19 @@
 const path = require('path');
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("sqlite:quiz.sqlite");
+const sequelize = new Sequelize("sqlite:quizzes.sqlite", {logging: false});
 
-sequelize.import(path.join(__dirname, 'quiz'));
+sequelize.define('quiz', {
+    question: {
+        type: Sequelize.STRING,
+        unique: {msg: "Ya existe esta pregunta"},
+        validate: {notEmpty: {msg: "La pregunta no puede estar vacía"}}
+    },
+    answer: {
+        type: Sequelize.STRING,
+        validate: {notEmpty: {msg: "La respuesta no puede estar vacía"}}
+    }
+});
 
 
 sequelize.sync()
